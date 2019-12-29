445cababb4b5a8051d3482a215f8c9afcec32bda
Joao Coelho <joao.coelho@bontouch.com>
2019-12-29 20:50:16 +0100

    no message

---
+++ src/main/java/unn/morphing/Morpher.java
@@ -0,0 +1,64 @@
+package unn.morphing;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import unn.interfaces.IOperator;
+import unn.mining.Model;
+import unn.structures.Config;
+import utils.RandomManager;
+
+public class Morpher {
+	Model modelFrom;
+	Model modelTo;
+
+	public Morpher() {
+		
+	}
+	
+	public void init(Model modelFrom, Model modelTo) {
+		this.modelFrom = modelFrom;
+		this.modelTo = modelTo;
+	}
+	
+	public void run(HashMap<IOperator, Integer> inputs, Integer totalVariation) {
+		ArrayList<IOperator> operators = new ArrayList<IOperator>();
+		operators.addAll(inputs.keySet());
+		
+		Collections.shuffle(operators);
+		
+		HashMap<IOperator, Integer> newInput = new HashMap<IOperator, Integer>();
+		Integer missingVariation = totalVariation;
+		int n = 0;
+		
+		
+		while (missingVariation > 0) {
+			int gradient = RandomManager.rand(-missingVariation, missingVariation);
+			
+			IOperator op = operators.get(n);
+			Integer val = inputs.get(op);
+			Integer newVal = val + gradient;
+			
+			if (newVal < Config.STIMULI_MIN_VALUE) {
+				newVal = Config.STIMULI_MIN_VALUE;
+			}
+			
+			if (newVal > Config.STIMULI_MAX_VALUE) {
+				newVal = Config.STIMULI_MAX_VALUE;
+			}
+			
+			missingVariation -= Math.abs(gradient);
+			newInput.put(op, newVal);
+			n = (n + 1) % operators.size();
+		}
+		
+		for (IOperator op : operators) {
+			if (!newInput.containsKey(op)) {
+				newInput.put(op, inputs.get(op));
+			}
+		}
+		
+		Double outcome = modelFrom.predict(newInput, null, null);
+	}
+}
