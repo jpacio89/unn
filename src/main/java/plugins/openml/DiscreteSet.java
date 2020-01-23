c5ca20ab33054f9b38c49acaefddf82f1073c0ef
Joao Coelho <joao.coelho@bontouch.com>
2020-01-23 23:35:10 +0100

    no message

--- src/main/java/plugins/openml/DiscreteSet.java
+++ src/main/java/plugins/openml/DiscreteSet.java
@@ -26,9 +26,13 @@ public class DiscreteSet extends OuterValueType {
 	public ArrayList<Integer> getAllInnerValues() {
 		ArrayList<Integer> innerValues = new ArrayList<Integer>();
 		
-		for (int i = 0; i < this.values.size(); ++i) {
-			int innerValue = (int) (Config.STIMULI_MIN_VALUE + i * Math.floor((Config.STIMULI_MAX_VALUE - Config.STIMULI_MIN_VALUE) / (cardinal() - 1)));
-			innerValues.add(innerValue);
+		if (cardinal() == 1) {
+			innerValues.add(Config.STIMULI_MIN_VALUE);
+		} else {
+			for (int i = 0; i < this.values.size(); ++i) {
+				int innerValue = (int) (Config.STIMULI_MIN_VALUE + i * Math.floor((Config.STIMULI_MAX_VALUE - Config.STIMULI_MIN_VALUE) / (cardinal() - 1)));
+				innerValues.add(innerValue);
+			}	
 		}
 		
 		return innerValues;
