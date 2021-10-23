19a1584d7de7e12c8ea821f32792c54de2ef0a61
Joao Coelho <joao.coelho@bontouch.com>
2021-10-23 17:44:13 +0100

    no message

--- src/test/java/com/unn/engine/TestInnerDatasetLoader.java
+++ src/test/java/com/unn/engine/TestInnerDatasetLoader.java
@@ -1,57 +1,64 @@
 package com.unn.engine;
 
+import com.unn.engine.data.Datasets;
 import com.unn.engine.dataset.InnerDataset;
 import com.unn.engine.dataset.InnerDatasetLoader;
 import com.unn.engine.dataset.OuterDataset;
+import com.unn.engine.interfaces.IFunctor;
+import com.unn.engine.metadata.NumericValuesDescriptor;
+import com.unn.engine.metadata.ValueMapper;
 import com.unn.engine.mining.models.JobConfig;
 import com.unn.engine.session.Context;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
 public class TestInnerDatasetLoader {
-    // InnerDatasetLoader is a key component in the engine
-    // as it is responsible for transforming any inputted dataset (OuterDataset)
-    // into a dataset (InnerDataset) that can be digested by the engine:
-    //   - converting labels into numerics
-    //   - clustering numerics
-    //   - mapping mixed values (labels and numerics)
-    private OuterDataset getOuterDataset() {
-        // TODO: get outer dataset
-        return null;
-    }
-
-    private Context getContext() {
-        // TODO: get context
-        return null;
-    }
-
-    private JobConfig getJob() {
-        // TODO: get job
-        return null;
+    String[] getNumericParts(String suffix, String group) {
+        return group
+            .replace("numeric_", "")
+            .replace(suffix, "")
+            .split("_");
     }
 
     @Test
-    public void testBasicFlow() {
-        OuterDataset outerDataset = getOuterDataset();
-        Context context = getContext();
-        JobConfig job = getJob();
+    public void test() {
+        OuterDataset outerDataset = Datasets.dummy1();
         InnerDatasetLoader loader = new InnerDatasetLoader();
         loader.init(outerDataset);
         InnerDataset innerDataset = loader.load();
+        ValueMapper mapper = loader.getValueMapper();
+
+        Arrays.stream(new String[]{"x", "y", "z"}).forEach(feature -> {
+            NumericValuesDescriptor descriptor = (NumericValuesDescriptor)
+                mapper.getValuesDescriptorByFeature(feature);
+
+            for (int i = 0; i < outerDataset.sampleCount(); ++i) {
+                HashMap<String, String> sample = outerDataset.getSampleAsMap(i);
+                int primer = Integer.parseInt(sample.get("primer"));
+                String outerValue = sample.get(feature);
+                ArrayList<String> activatedGroups = descriptor.getGroupByOuterValue(outerValue);
+
+                activatedGroups.forEach(group -> {
+                    IFunctor functor = descriptor.getFunctorByGroup(group);
+                    int innerValue = innerDataset.getValueByTime(functor, primer);
+                    assertEquals(innerValue, Config.STIM_MAX);
+                });
+
+                descriptor.getGroups().stream()
+                    .filter(e -> !activatedGroups.contains(e))
+                    .forEach(group -> {
+                        IFunctor functor = descriptor.getFunctorByGroup(group);
+                        int innerValue = innerDataset.getValueByTime(functor, primer);
+                        assertEquals(innerValue, Config.STIM_MIN);
+                    });
+            }
+        });
+
         // TODO: do checks
     }
-
-    @Test
-    public void testReuseValueMapper() {
-        // TODO: get outer dataset
-        // TODO: create InnerDatasetLoader instance
-        // TODO: convert
-        // TODO: get ValueMapper
-        // TODO: convert second dataset using value mapper
-        //  (add unknown outer values not found in dataset 1)
-    }
-
-    // TODO: test discrete mapping
-    // TODO: test numeric mapping
-    // TODO: test mixed mapping
-    // TODO: test feature binarization
 }
