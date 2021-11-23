4f9a1c7b4d7224cdb1563eeb8803052bdd98d990
Joao Coelho <joao.coelho@bontouch.com>
2021-11-23 13:58:10 +0000

    no message

--- src/test/java/com/unn/engine/TestStocks.java
+++ src/test/java/com/unn/engine/TestStocks.java
@@ -5,33 +5,34 @@ import com.unn.stocks.InputMiner;
 import org.junit.Test;
 
 public class TestStocks {
+    private String instrumentId = "1003";
     private static String folderPath = "/Volumes/Legatron/data/serializations/stock-market-crawler/batch-mining";
 
     @Test
-    public void testInputMiner_target1001() {
-        InputMiner.runAll(String.format("%s/target-1001", folderPath), 1);
+    public void testInputMiner_target() {
+        InputMiner.runAll(String.format("%s/target-%s", folderPath, instrumentId), 1);
     }
 
     @Test
-    public void testInputBatchPredictor_target1001() {
-        new InputBatchPredictor(String.format("%s/target-1001", folderPath), "1001", false)
+    public void testInputBatchPredictor_target() {
+        new InputBatchPredictor(String.format("%s/target-%s", folderPath, instrumentId), instrumentId, false)
             .start();
     }
 
     @Test
     public void testMineOutputLayer() {
-        InputMiner.runAll(String.format("%s/target-1001/output", folderPath), 2);
+        InputMiner.runAll(String.format("%s/target-%s/output", folderPath, instrumentId), 2);
     }
 
     @Test
-    public void testInputBatchRealtimePredictor_target1001() {
-        new InputBatchPredictor(String.format("%s/target-1001", folderPath), "1001", true)
+    public void testInputBatchRealtimePredictor_target() {
+        new InputBatchPredictor(String.format("%s/target-%s", folderPath, instrumentId), instrumentId, true)
                 .start();
     }
 
     @Test
     public void testOutputBatchRealtimePredictor_target1001() {
-        new InputBatchPredictor(String.format("%s/target-1001/output", folderPath), "1", true)
+        new InputBatchPredictor(String.format("%s/target-%s/output", folderPath, instrumentId), "1", true)
                 .start();
     }
 }
