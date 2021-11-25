a8d4093edd47349b1bfcae236e68fe3335d61449
Joao Coelho <joao.coelho@bontouch.com>
2021-11-25 21:50:17 +0000

    no message

--- src/main/java/com/unn/stocks/StockAnalyzerApp.java
+++ src/main/java/com/unn/stocks/StockAnalyzerApp.java
@@ -1,13 +1,17 @@
 package com.unn.stocks;
 
+import com.unn.common.utils.ZipHelper;
 import com.unn.engine.Config;
+
+import java.io.*;
 
 public class StockAnalyzerApp {
     private static String basePath = "/Volumes/Legatron/data/serializations/stock-market-crawler/batch-mining";
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException, InterruptedException {
         basePath = args[0];
         String targetInstrumentId = args[1];
+        String uploadUrl = args[2];
         String folderPath = String.format("%s/target-%s", basePath, targetInstrumentId);
 
         System.out.println(String.format("|StockAnalyzerApp| Analyzing instrument %s", targetInstrumentId));
@@ -28,19 +32,43 @@ public class StockAnalyzerApp {
 
         Config.get().MODEL_PREDICTION_PREDICATE_HIT_COUNT = 30;
 
-        InputMiner.runAll(String.format("%s/output", folderPath), 2);
-
         System.out.println(String.format("|StockAnalyzerApp| Gathering layer 1 realtime outputs"));
 
         new InputBatchPredictor(folderPath, targetInstrumentId, true)
-            .start();
+                .start();
+
+        InputMiner.runAll(String.format("%s/output", folderPath), 2);
 
         System.out.println(String.format("|StockAnalyzerApp| Gathering layer 2 realtime outputs"));
 
         new InputBatchPredictor(String.format("%s/output", folderPath), "1", true)
             .start();
 
+        System.out.println(String.format("|StockAnalyzerApp| Zipping deliverables"));
+
+        String zipPath = String.format("%s/target-%s.zip", folderPath, targetInstrumentId);
+        ZipHelper.zip(folderPath, zipPath);
+
+        System.out.println(String.format("|StockAnalyzerApp| Uploading deliverables"));
+
+        uploadDeliverables(zipPath, uploadUrl);
     }
 
+    private static void uploadDeliverables(String zipPath, String uploadUrl) throws IOException, InterruptedException {
+        File zipFile = new File(zipPath);
+        String[] command = new String[] {
+            "curl", "-F", String.format("file=@%s", zipFile.getAbsolutePath()), uploadUrl
+        };
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.redirectErrorStream(true);
+        Process p = pb.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
+        for (String line; (line = reader.readLine()) != null;) {
+            System.out.println(line);
+        }
+
+        p.waitFor();
+        reader.close();
+    }
 }
