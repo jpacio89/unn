fdcda6c88f7ed3c33977e540c3e40fbf6646cad2
Joao Coelho <joao.coelho@bontouch.com>
2021-12-12 15:53:27 +0000

    no message

--- src/main/java/com/unn/stocks/ArchiveReportAnalyzer.java
+++ src/main/java/com/unn/stocks/ArchiveReportAnalyzer.java
@@ -1,11 +1,14 @@
 package com.unn.stocks;
 
+import com.unn.common.mining.ConfusionMatrix;
 import com.unn.common.mining.MiningReport;
 import com.unn.common.utils.Serializer;
 import com.unn.engine.session.Session;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ArchiveReportAnalyzer {
     private static String archivePath = "/Volumes/Legatron/data/serializations/stock-market-crawler/targets-archive";
@@ -16,9 +19,23 @@ public class ArchiveReportAnalyzer {
 
         for (File reportFile : files) {
             MiningReport report = (MiningReport) Serializer.read(reportFile.getAbsolutePath().replace(".v1.report", ""), "report");
+            if (!ofInterest(report)) {
+                continue;
+            }
             System.out.println(reportFile.getAbsolutePath());
             System.out.println(report.toString());
         }
+    }
 
+    private static boolean ofInterest(MiningReport report) {
+        ArrayList<String> keys = report.confusionMatrixes.keySet().stream()
+            .filter(key -> key.contains("Over:"))
+            .collect(Collectors.toCollection(ArrayList::new));
+        if (keys.size() == 0) {
+            return false;
+        }
+        String aboveKey = keys.get(0);
+        ConfusionMatrix matrix = report.getConfusionMatrixes().get(aboveKey);
+        return matrix.getTpCount() > 20 && matrix.getTpr() > 80;
     }
 }
